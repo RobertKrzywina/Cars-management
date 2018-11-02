5f8c06348cebe34452d812ddee08693b3758abfc
RobertK <robert.krzywina@gmail.com>
2018-11-02 11:39:24 +0100

    first commit

---
+++ src/main/java/pl/robert/springrestproject/SpringrestProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.robert.springrestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestProjectApplication.class, args);
+	}
+}
